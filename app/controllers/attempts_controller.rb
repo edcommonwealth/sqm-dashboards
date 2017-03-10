43140f8ec9481eede11c8bfe9f0d2ae7971ecc1f
Jared Cosulich <jared.cosulich@gmail.com>
2017-03-10 16:11:08 -0500

    working on twilio integration

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -3,7 +3,7 @@ class AttemptsController < ApplicationController
   protect_from_forgery :except => [:twilio]
 
   def twilio
-    attempt = Attempt.where(twilio_sid: params['From']).first
+    attempt = Attempt.where(from: params['From']).first
     attempt.update_attributes(
       answer_index: params[:Body].to_i,
       twilio_details: params.to_h.to_yaml
