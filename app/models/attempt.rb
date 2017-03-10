8b19e1fac2a1e505cced6b79b700f5e842104bbf
Jared Cosulich <jared.cosulich@gmail.com>
2017-03-10 15:55:33 -0500

    fixing twilio message

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -14,7 +14,7 @@ class Attempt < ApplicationRecord
     message = client.messages.create(
       from: twilio_number,
       to:   recipient.phone,
-      body: "#{question.text}%0a%0a#{question.option1}: Reply 1\n\r%0a%Oa#{question.option2}: Reply 2%0a#{question.option3}: Reply 3%0a#{question.option4}: Reply 4%0a#{question.option5}: Reply 5"
+      body: "#{question.text}\n\r#{question.option1}: Reply 1\n\r#{question.option2}: Reply 2\n\r#{question.option3}: Reply 3\n\r#{question.option4}: Reply 4\n\r#{question.option5}: Reply 5"
     )
 
     puts message.inspect
