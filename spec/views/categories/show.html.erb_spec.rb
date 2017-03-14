2a5c88d9cf9b91c42b5872a4c71c374b990d8f7a
Jared Cosulich <jared.cosulich@gmail.com>
2017-03-14 11:27:34 -0400

    fixing tests

--- spec/views/categories/show.html.erb_spec.rb
+++ spec/views/categories/show.html.erb_spec.rb
@@ -2,21 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "categories/show", type: :view do
   before(:each) do
+    @school = assign(:school, School.create(name: 'School'))
     @category = assign(:category, Category.create!(
-      :name => "Name",
-      :blurb => "Blurb",
+      :name => "Category",
       :description => "MyText",
-      :external_id => "External",
-      :parent_category => Category.create(name: 'Parent Category')
+      :parent_category => Category.create(name: 'Teachers And The Teaching Environment')
     ))
+    @school_category = assign(:school_category, SchoolCategory.create(school: @school, category: @category))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Blurb/)
+    expect(rendered).to match(/Category/)
     expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/External/)
-    expect(rendered).to match(/Parent Category/)
+    expect(rendered).to match(/Teachers And The Teaching Environment/)
   end
 end
