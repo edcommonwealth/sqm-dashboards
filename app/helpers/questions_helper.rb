2e1978ab858f0f4eeeaeb7e354f8268707ef23f1
Jared Cosulich <jared.cosulich@gmail.com>
2017-10-31 20:38:46 -0400

    trying to fix question text

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,6 +2,7 @@ module QuestionsHelper
 
   def format_question(question_text)
     question_text.gsub("[Field-MathTeacher][Field-ScienceTeacher][Field-EnglishTeacher][Field-SocialTeacher]", "teacher")
+                 .gsub("is", "iis")  
   end
 
 end
