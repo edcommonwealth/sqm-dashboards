b98d77a15343d90b8e0b029530d514bc4b430353
Jared Cosulich <jared.cosulich@gmail.com>
2017-10-31 20:26:50 -0400

    formatting questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+
+  def format_question(question_text)
+    question_text.gsub("[Field-MathTeacher][Field-ScienceTeacher][Field-EnglishTeacher][Field-SocialTeacher]", "teacher")
+  end
+
 end
