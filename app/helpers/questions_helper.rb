712d8baa18bf265a80a4b3bdeeafd115b4449b04
Jared Cosulich <jared.cosulich@gmail.com>
2017-10-31 20:45:45 -0400

    removing test

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,6 @@ module QuestionsHelper
 
   def format_question(question_text)
     question_text.gsub("[Field-MathTeacher][Field-ScienceTeacher][Field-EnglishTeacher][Field-SocialTeacher]", "teacher")
-                 .gsub("is", "iis")  
   end
 
 end
