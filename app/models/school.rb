06e733d125cb331b67ac9a5a5ca0888098835fcd
Jared Cosulich <jared.cosulich@gmail.com>
2017-11-02 12:34:19 -0400

    school merging tool

--- app/models/school.rb
+++ app/models/school.rb
@@ -4,6 +4,7 @@ class School < ApplicationRecord
   belongs_to :district
   has_many :recipients, dependent: :destroy
   has_many :school_categories, dependent: :destroy
+  has_many :user_schools, dependent: :destroy
 
   validates :name, presence: true
 
@@ -11,5 +12,15 @@ class School < ApplicationRecord
 
   include FriendlyId
   friendly_id :name, :use => [:slugged]
+
+  def merge_into(school)
+    puts "Merging #{name} (#{id}) in to #{school.name} (#{school.id})"
+    schedules.update_all(school: school)
+    recipient_lists.update_all(school: school)
+    recipients.update_all(school: school)
+    school_categories.update_all(school: school)
+    user_schools.update_all(school: school)
+    destroy
+  end
 
 end
