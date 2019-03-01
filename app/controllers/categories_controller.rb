56157a612d45a019329593b187e70620b0e00f15
Jared Cosulich <jared.cosulich@gmail.com>
2019-03-01 13:04:42 -0500

    don't show admin data if that category is missing on a page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,6 +27,7 @@ class CategoriesController < ApplicationController
     @child_school_categories = SchoolCategory.for_parent_category(@school, @category).in(@year).valid.to_a
     missing_categories = Category.for_parent(@category) - @child_school_categories.map(&:category)
     missing_categories.each do |category|
+      next if category.benchmark.present?
       @child_school_categories << category.school_categories.new(school: @school)
     end
     @questions = @category.questions.created_in(@year)
