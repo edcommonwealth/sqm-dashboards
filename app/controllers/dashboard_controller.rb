8780893d21e33ef224e7174d2c4ba0ea1531b12a
Alex Basson <abasson@vmware.com>
2021-09-22 14:15:41 -0400

    Extract method_ids as private method in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,23 @@
 class DashboardController < ApplicationController
   def index
     authenticate(district.name.downcase, "#{district.name.downcase}!")
-    @measure_graph_row_presenters = Measure.where(measure_id: '1A-i').map do | measure |
-      MeasureGraphRowPresenter.new(
-        measure: measure,
-        score: SurveyResponseAggregator.score(school: school, academic_year: academic_year, measure: measure)
-      )
-    end
+    @measure_graph_row_presenters = measure_ids
+                                      .map { |measure_id| Measure.find_by_measure_id measure_id }
+                                      .map(&method(:presenter_for_measure))
   end
 
   private
+
+  def measure_ids
+    %w(1A-i)
+  end
+
+  def presenter_for_measure(measure)
+    MeasureGraphRowPresenter.new(
+      measure: measure,
+      score: SurveyResponseAggregator.score(school: school, academic_year: academic_year, measure: measure)
+    )
+  end
 
   def school
     @school ||= School.find_by_slug school_slug
