18ddfaaaf807ab675a87dc3fbbb52643dd750bd1
Alex Basson <abasson@vmware.com>
2021-09-21 20:39:43 -0400

    Optimize SurveyResponseAggregator via a join
    [Finishes #179425717]

--- app/services/survey_response_aggregator.rb
+++ app/services/survey_response_aggregator.rb
@@ -8,8 +8,8 @@ class SurveyResponseAggregator
   def self.score(academic_year:, school:, measure:)
     SurveyItemResponse
       .where(academic_year: academic_year, school: school)
-      .filter { |survey_item_response| survey_item_response.survey_item.measure == measure }
-      .map { |survey_response| survey_response.likert_score }
+      .joins(:survey_item).where('survey_items.measure_id': measure.id)
+      .map { |survey_item_response| survey_item_response.likert_score }
       .average
   end
 end
