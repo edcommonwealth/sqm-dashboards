5fc9e7b3cc8c208338334cacb4781fb98dd6ab6d
Liam Morley <lmorley@vmware.com>
2021-10-25 08:53:15 -0400

    move watch script to package.json

--- scripts/bundling.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-set -eux
-
-yarn sass ./app/assets/stylesheets/application.bootstrap.scss ./app/assets/builds/application.css --no-source-map --load-path=node_modules --watch & yarn sass ./app/assets/stylesheets/sqm_application.scss ./app/assets/builds/sqm_application.css --no-source-map --load-path=node_modules --watch & npx sass ./app/assets/stylesheets/welcome.scss ./app/assets/builds/welcome.css --no-source-map --load-path=node_modules --watch & yarn build --watch 
