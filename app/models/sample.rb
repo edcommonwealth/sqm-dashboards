191abdbd501ddc415d4d239db72453c2b89674ca
rebuilt <memoryman51@hotmail.com>
2022-08-15 13:46:19 -0700

    Align styling to closer match design

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,12 +1,13 @@
 class Sample
   attr_reader :school, :academic_year, :category, :measure, :race
 
-  def initialize
-    @school = School.find_by_slug 'milford-high-school'
-    @academic_year = AcademicYear.last
-    @category = Category.find_by_category_id '1'
-    @subcategory = Subcategory.find_by_subcategory_id '1A'
-    @measure = Measure.find_by_measure_id '1A-ii'
-    @race = Race.find_by_qualtrics_code 1
+  def initialize(slug: 'milford-high-school', range: '2021-22', category_id: '1', subcategory_id: '1A',
+                 measure_id: '1A-ii', race_code: 1)
+    @school = School.find_by_slug slug
+    @academic_year = AcademicYear.find_by_range range
+    @category = Category.find_by_category_id category_id
+    @subcategory = Subcategory.find_by_subcategory_id subcategory_id
+    @measure = Measure.find_by_measure_id measure_id
+    @race = Race.find_by_qualtrics_code race_code
   end
 end
