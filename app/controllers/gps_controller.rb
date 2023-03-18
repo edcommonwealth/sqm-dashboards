86e67d39650c3a93464c75d82268b8ac4ada9500
rebuilt <memoryman51@hotmail.com>
2023-03-18 13:45:11 -0700

    Temporarily disable reporting because its crashing production.  No data has been seeded and the logs are reporting this class as causing the crash?

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,8 +1,8 @@
 class GpsController < ActionController::Base
   def index
-    respond_to do |format|
-      format.html
-      format.csv { send_data Report::Gps.to_csv, disposition: 'attachment', filename: "gps_#{Date.today}.csv" }
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.csv { send_data Report::Gps.to_csv, disposition: 'attachment', filename: "gps_#{Date.today}.csv" }
+    # end
   end
 end
