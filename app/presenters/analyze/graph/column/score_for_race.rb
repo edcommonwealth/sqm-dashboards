d48a4efda28d2ad9f6e16d8c2540f06e88aae869
rebuilt <memoryman51@hotmail.com>
2023-05-12 13:08:25 -0700

    Round all averages to two decimal places

--- app/presenters/analyze/graph/column/score_for_race.rb
+++ app/presenters/analyze/graph/column/score_for_race.rb
@@ -4,7 +4,7 @@ module Analyze
       module ScoreForRace
         def score(year_index)
           s = ::RaceScore.find_by(measure:, school:, academic_year: academic_years[year_index], race:)
-          average = s.average unless s.nil?
+          average = s.average.round(2) unless s.nil?
           average ||= 0
           meets_student_threshold = s.meets_student_threshold? unless s.nil?
           meets_student_threshold ||= false
