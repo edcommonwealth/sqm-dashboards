dd94b283faba41fc15f6fdf9e8e2591f49bf538d
rebuilt <memoryman51@hotmail.com>
2023-05-15 14:55:12 -0700

    Remove hotjar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_google_analytics_id
-  before_action :set_hotjar_id
 
   def set_google_analytics_id
     @google_analytics_id = ENV['GOOGLE_ANALYTICS_ID']
-  end
-
-  def set_hotjar_id
-    @hotjar_id = ENV['HOTJAR_ID']
   end
 end
