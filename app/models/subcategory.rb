b5dc933187c5ab61daa16b6c31539a2d34dc05a7
rebuilt <memoryman51@hotmail.com>
2023-05-19 16:50:10 -0700

    Add subcategory report

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -7,11 +7,27 @@ class Subcategory < ActiveRecord::Base
   has_many :survey_items, through: :measures
 
   def score(school:, academic_year:)
-    scores = measures.map do |measure|
+    measures.map do |measure|
       measure.score(school:, academic_year:).average
-    end
-    scores = scores.reject(&:nil?)
-    scores.average
+    end.remove_blanks.average
+  end
+
+  def student_score(school:, academic_year:)
+    measures.map do |measure|
+      measure.student_score(school:, academic_year:).average
+    end.remove_blanks.average
+  end
+
+  def teacher_score(school:, academic_year:)
+    measures.map do |measure|
+      measure.teacher_score(school:, academic_year:).average
+    end.remove_blanks.average
+  end
+
+  def admin_score(school:, academic_year:)
+    measures.map do |measure|
+      measure.admin_score(school:, academic_year:).average
+    end.remove_blanks.average
   end
 
   def response_rate(school:, academic_year:)
@@ -24,5 +40,52 @@ class Subcategory < ActiveRecord::Base
     end
 
     @response_rate[[school, academic_year]]
+  end
+
+  def warning_low_benchmark
+    1
+  end
+
+  def watch_low_benchmark
+    @watch_low_benchmark ||= benchmark(:watch_low_benchmark)
+  end
+
+  def growth_low_benchmark
+    @growth_low_benchmark ||= benchmark(:growth_low_benchmark)
+  end
+
+  def approval_low_benchmark
+    @approval_low_benchmark ||= benchmark(:approval_low_benchmark)
+  end
+
+  def ideal_low_benchmark
+    @ideal_low_benchmark ||= benchmark(:ideal_low_benchmark)
+  end
+
+  def benchmark(name)
+    measures.map do |measure|
+      measure.benchmark(name)
+    end.average
+  end
+
+  def zone(school:, academic_year:)
+    zone_for_score(score: score(school:, academic_year:))
+  end
+
+  def student_zone(school:, academic_year:)
+    zone_for_score(score: student_score(school:, academic_year:))
+  end
+
+  def teacher_zone(school:, academic_year:)
+    zone_for_score(score: teacher_score(school:, academic_year:))
+  end
+
+  def admin_zone(school:, academic_year:)
+    zone_for_score(score: admin_score(school:, academic_year:))
+  end
+
+  def zone_for_score(score:)
+    Zones.new(watch_low_benchmark:, growth_low_benchmark:,
+              approval_low_benchmark:, ideal_low_benchmark:).zone_for_score(score)
   end
 end
---
+++ spec/models/report/subcategory_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+RSpec.describe Report::Subcategory, type: :model do
+  let(:school) { create(:school, name: 'Milford High', slug: 'milford-high') }
+  let(:academic_year) { create(:academic_year, range: '2018-2019') }
+  let(:subcategory) { create(:subcategory, subcategory_id: '1A') }
+  let(:respondent) { create(:respondent, school:, academic_year:) }
+  before :each do
+    school
+    academic_year
+    subcategory
+    respondent
+  end
+  let(:measure) { create(:measure, subcategory:) }
+  let(:scale) { create(:scale, measure:) }
+  let(:survey_item) { create(:student_survey_item, scale:) }
+
+  context 'when creating a report for a subcategory' do
+    before :each do
+      create_list(:survey_item_response, 10, survey_item:, school:, academic_year:)
+    end
+
+    it 'creates a report for subcategories' do
+      expect(Report::Subcategory.create_report).to be_a(Array)
+      headers = Report::Subcategory.create_report.first
+      expect(headers).to eq(['School', 'Academic Year', 'Subcategory', 'Student Score',
+                             'Student Zone', 'Teacher Score', 'Teacher Zone', 'Admin Score', 'Admin Zone', 'All Score (Average)', 'All Score Zone'])
+    end
+
+    it 'Adds information about the first school and first academic year to the report' do
+      report = Report::Subcategory.create_report
+      report[1] in [school_name, academic_year, subcategory_id, *]
+      expect(school_name).to eq('Milford High')
+      expect(academic_year).to eq('2018-2019')
+      expect(subcategory_id).to eq('1A')
+    end
+  end
+  describe '.create_report' do
+    before do
+      allow(Report::Subcategory).to receive(:write_csv)
+    end
+
+    it 'generates a CSV report' do
+      expect(FileUtils).to receive(:mkdir_p).with(Rails.root.join('tmp', 'reports'))
+
+      Report::Subcategory.create_report
+
+      expect(Report::Subcategory).to have_received(:write_csv)
+    end
+
+    it 'returns the report data' do
+      data = Report::Subcategory.create_report
+
+      expect(data).to be_an(Array)
+    end
+  end
+  describe '.write_csv' do
+    it 'writes the data to a CSV file' do
+      data = [['School', 'Academic Year', 'Subcategory'], ['School A', '2022-2023', 'Category A']]
+      filepath = Rails.root.join('tmp', 'spec', 'reports', 'subcategories.csv')
+
+      FileUtils.mkdir_p Rails.root.join('tmp', 'spec', 'reports')
+      Report::Subcategory.write_csv(data:, filepath:)
+
+      csv_data = File.read(filepath)
+      expect(csv_data).to include('School,Academic Year,Subcategory')
+      expect(csv_data).to include('School A,2022-2023,Category A')
+    end
+  end
+
+  describe '.student_score' do
+    let(:response_rate) { create(:response_rate, subcategory:, school:, academic_year:) }
+    let(:row) { [response_rate, subcategory, school, academic_year] }
+
+    it 'returns student score if response rate meets student threshold' do
+      allow(subcategory).to receive(:student_score).and_return(80)
+      allow(response_rate).to receive(:meets_student_threshold?).and_return(true)
+
+      score = Report::Subcategory.student_score(row:)
+
+      expect(score).to eq(80)
+    end
+
+    it 'returns "N/A" if response rate does not meet student threshold' do
+      allow(response_rate).to receive(:meets_student_threshold?).and_return(false)
+
+      score = Report::Subcategory.student_score(row:)
+
+      expect(score).to eq('N/A')
+    end
+  end
+end
