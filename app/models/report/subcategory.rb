f29d72abff95d27fbddacd9950babf8166c2ea6a
rebuilt <memoryman51@hotmail.com>
2023-06-09 15:29:12 -0700

    Add District, School Code, and Grade columns to subcategory report

--- app/models/report/subcategory.rb
+++ app/models/report/subcategory.rb
@@ -1,13 +1,14 @@
 module Report
   class Subcategory
-    def self.create_report(schools: School.all, academic_years: AcademicYear.all, subcategories: ::Subcategory.all, filename: "subcategories.csv")
+    def self.create_report(schools: School.all, academic_years: AcademicYear.all, subcategories: ::Subcategory.all, filename: 'subcategories.csv')
       data = []
-      data << ["School", "Academic Year", "Subcategory", "Student Score", "Student Zone", "Teacher Score",
-        "Teacher Zone", "Admin Score", "Admin Zone", "All Score (Average)", "All Score Zone"]
+      data << ['District', 'School', 'School Code', 'Academic Year', 'Grades', 'Subcategory', 'Student Score', 'Student Zone', 'Teacher Score',
+               'Teacher Zone', 'Admin Score', 'Admin Zone', 'All Score (Average)', 'All Score Zone']
       schools.each do |school|
         academic_years.each do |academic_year|
           subcategories.each do |subcategory|
-            next if Respondent.where(school:, academic_year:).empty?
+            respondents = Respondent.find_by(school:, academic_year:)
+            next if respondents.nil?
 
             response_rate = subcategory.response_rate(school:, academic_year:)
             next unless response_rate.meets_student_threshold? || response_rate.meets_teacher_threshold?
@@ -16,23 +17,29 @@ module Report
             zone = subcategory.zone(school:, academic_year:).type.to_s.capitalize
 
             row = [response_rate, subcategory, school, academic_year]
-            data << [school.name,
-              academic_year.range,
-              subcategory.subcategory_id,
-              student_score(row:),
-              student_zone(row:),
-              teacher_score(row:),
-              teacher_zone(row:),
-              admin_score(row:),
-              admin_zone(row:),
-              score,
-              zone]
+
+            all_grades = respondents.counts_by_grade.keys
+            grades = "#{all_grades.first}-#{all_grades.last}"
+            data << [school.district.name,
+                     school.name,
+                     school.dese_id,
+                     academic_year.range,
+                     grades,
+                     subcategory.subcategory_id,
+                     student_score(row:),
+                     student_zone(row:),
+                     teacher_score(row:),
+                     teacher_zone(row:),
+                     admin_score(row:),
+                     admin_zone(row:),
+                     score,
+                     zone]
           end
         end
       end
 
-      FileUtils.mkdir_p Rails.root.join("tmp", "reports")
-      filepath = Rails.root.join("tmp", "reports", filename)
+      FileUtils.mkdir_p Rails.root.join('tmp', 'reports')
+      filepath = Rails.root.join('tmp', 'reports', filename)
       write_csv(data:, filepath:)
       data
     end
@@ -49,24 +56,24 @@ module Report
     def self.student_score(row:)
       row in [response_rate, subcategory, school, academic_year]
       student_score = subcategory.student_score(school:, academic_year:) if response_rate.meets_student_threshold?
-      student_score || "N/A"
+      student_score || 'N/A'
     end
 
     def self.student_zone(row:)
       row in [response_rate, subcategory, school, academic_year]
       if response_rate.meets_student_threshold?
         student_zone = subcategory.student_zone(school:,
-          academic_year:).type.to_s.capitalize
+                                                academic_year:).type.to_s.capitalize
       end
 
-      student_zone || "N/A"
+      student_zone || 'N/A'
     end
 
     def self.teacher_score(row:)
       row in [response_rate, subcategory, school, academic_year]
       teacher_score = subcategory.teacher_score(school:, academic_year:) if response_rate.meets_teacher_threshold?
 
-      teacher_score || "N/A"
+      teacher_score || 'N/A'
     end
 
     def self.teacher_zone(row:)
@@ -75,20 +82,20 @@ module Report
         teacher_zone = subcategory.teacher_zone(school:, academic_year:).type.to_s.capitalize
       end
 
-      teacher_zone || "N/A"
+      teacher_zone || 'N/A'
     end
 
     def self.admin_score(row:)
       row in [response_rate, subcategory, school, academic_year]
       admin_score = subcategory.admin_score(school:, academic_year:)
-      admin_score = "N/A" unless admin_score >= 0
+      admin_score = 'N/A' unless admin_score >= 0
       admin_score
     end
 
     def self.admin_zone(row:)
       row in [response_rate, subcategory, school, academic_year]
       tmp_zone = subcategory.admin_zone(school:, academic_year:).type
-      (tmp_zone == :insufficient_data) ? "N/A" : tmp_zone.to_s.capitalize
+      tmp_zone == :insufficient_data ? 'N/A' : tmp_zone.to_s.capitalize
     end
   end
 end
