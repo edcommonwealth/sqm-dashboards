7cf6c6cc241059a5707c558ebaf54a2993bde528
rebuilt <memoryman51@hotmail.com>
2023-06-09 16:58:51 -0700

    Modify subcategory report to be threaded

--- app/models/report/subcategory.rb
+++ app/models/report/subcategory.rb
@@ -1,43 +1,56 @@
 module Report
   class Subcategory
-    def self.create_report(schools: School.all, academic_years: AcademicYear.all, subcategories: ::Subcategory.all, filename: 'subcategories.csv')
+    def self.create_report(schools: School.all.includes(:district), academic_years: AcademicYear.all, subcategories: ::Subcategory.all, filename: 'subcategories.csv')
       data = []
+      mutex = Thread::Mutex.new
       data << ['District', 'School', 'School Code', 'Academic Year', 'Grades', 'Subcategory', 'Student Score', 'Student Zone', 'Teacher Score',
                'Teacher Zone', 'Admin Score', 'Admin Zone', 'All Score (Average)', 'All Score Zone']
-      schools.each do |school|
-        academic_years.each do |academic_year|
-          subcategories.each do |subcategory|
-            respondents = Respondent.find_by(school:, academic_year:)
-            next if respondents.nil?
+      pool_size = 2
+      jobs = Queue.new
+      schools.each { |school| jobs << school }
 
-            response_rate = subcategory.response_rate(school:, academic_year:)
-            next unless response_rate.meets_student_threshold? || response_rate.meets_teacher_threshold?
+      workers = pool_size.times.map do
+        Thread.new do
+          while school = jobs.pop(true)
+            academic_years.each do |academic_year|
+              subcategories.each do |subcategory|
+                respondents = Respondent.find_by(school:, academic_year:)
+                next if respondents.nil?
 
-            score = subcategory.score(school:, academic_year:)
-            zone = subcategory.zone(school:, academic_year:).type.to_s.capitalize
+                response_rate = subcategory.response_rate(school:, academic_year:)
+                next unless response_rate.meets_student_threshold? || response_rate.meets_teacher_threshold?
 
-            row = [response_rate, subcategory, school, academic_year]
+                score = subcategory.score(school:, academic_year:)
+                zone = subcategory.zone(school:, academic_year:).type.to_s.capitalize
 
-            all_grades = respondents.counts_by_grade.keys
-            grades = "#{all_grades.first}-#{all_grades.last}"
-            data << [school.district.name,
-                     school.name,
-                     school.dese_id,
-                     academic_year.range,
-                     grades,
-                     subcategory.subcategory_id,
-                     student_score(row:),
-                     student_zone(row:),
-                     teacher_score(row:),
-                     teacher_zone(row:),
-                     admin_score(row:),
-                     admin_zone(row:),
-                     score,
-                     zone]
+                row = [response_rate, subcategory, school, academic_year]
+
+                all_grades = respondents.counts_by_grade.keys
+                grades = "#{all_grades.first}-#{all_grades.last}"
+                mutex.synchronize do
+                  data << [school.district.name,
+                           school.name,
+                           school.dese_id,
+                           academic_year.range,
+                           grades,
+                           subcategory.subcategory_id,
+                           student_score(row:),
+                           student_zone(row:),
+                           teacher_score(row:),
+                           teacher_zone(row:),
+                           admin_score(row:),
+                           admin_zone(row:),
+                           score,
+                           zone]
+                end
+              end
+            end
           end
+        rescue ThreadError
         end
       end
 
+      workers.each(&:join)
       FileUtils.mkdir_p Rails.root.join('tmp', 'reports')
       filepath = Rails.root.join('tmp', 'reports', filename)
       write_csv(data:, filepath:)
