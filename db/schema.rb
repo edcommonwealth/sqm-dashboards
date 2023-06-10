8a23296ca5ee41e671861ed2206bb1b33b27ba21
rebuilt <memoryman51@hotmail.com>
2023-06-10 10:19:05 -0700

    perf: Add another index to surveyitemresponses.   Implement bullet suggestions.  Memoize a function in measure.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_17_191736) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_165508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -448,6 +448,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_17_191736) do
     t.index ["academic_year_id"], name: "index_survey_item_responses_on_academic_year_id"
     t.index ["gender_id"], name: "index_survey_item_responses_on_gender_id"
     t.index ["response_id"], name: "index_survey_item_responses_on_response_id"
+    t.index ["school_id", "academic_year_id", "survey_item_id"], name: "by_school_year_and_survey_item"
     t.index ["school_id", "academic_year_id"], name: "index_survey_item_responses_on_school_id_and_academic_year_id"
     t.index ["school_id", "survey_item_id", "academic_year_id", "grade"], name: "index_survey_responses_on_grade"
     t.index ["school_id"], name: "index_survey_item_responses_on_school_id"
