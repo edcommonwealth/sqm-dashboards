b06d90a6e2d27e8e04203262eaf2698ae011d9e8
rebuilt <memoryman51@hotmail.com>
2023-07-10 13:45:25 -0700

    chore: switch from apparition to cuprite for integration tests.  Disable journey spec for the moment

--- spec/system/sqm_application_spec.rb
+++ spec/system/sqm_application_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'SQM Application' do
+describe "SQM Application" do
   let(:district) { create(:district) }
   let(:school) { create(:school, district:) }
   let(:academic_year) { create(:academic_year) }
@@ -17,29 +17,29 @@ describe 'SQM Application' do
                          student_response_rate: 0, teacher_response_rate: 0, meets_student_threshold: false, meets_teacher_threshold: false)
   end
 
-  context 'when no measures meet their threshold' do
-    it 'shows a modal on overview page' do
+  context "when no measures meet their threshold" do
+    it "shows a modal on overview page" do
       visit overview_path
-      expect(page).to have_css '.modal'
+      expect(page).to have_css ".modal"
     end
 
-    it 'does not show a modal on the browse page' do
+    it "does not show a modal on the browse page" do
       visit browse_path
-      expect(page).not_to have_css '.modal'
+      expect(page).not_to have_css ".modal"
     end
   end
 
-  context 'at least one measure meets its threshold' do
+  context "at least one measure meets its threshold" do
     before :each do
       teacher_survey_item = create(:teacher_survey_item, scale:)
       create_list(:survey_item_response, SurveyItemResponse::TEACHER_RESPONSE_THRESHOLD,
                   survey_item: teacher_survey_item, academic_year:, school:)
     end
 
-    it 'does not show a modal on any page' do
+    it "does not show a modal on any page" do
       [overview_path, browse_path].each do |path|
         visit path
-        expect(page).not_to have_css '.modal'
+        expect(page).not_to have_css ".modal"
       end
     end
   end
