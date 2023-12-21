9e368ae47db9aca5fa16f77593cdfdb33caa7f16
Nelson Jovel <memoryman51@hotmail.com>
2023-12-21 13:54:54 -0800

    chore: prevent multiple lookups for the boston district by inlining the sql

--- app/models/survey_item_response.rb
+++ app/models/survey_item_response.rb
@@ -16,8 +16,7 @@ class SurveyItemResponse < ActiveRecord::Base
   has_one :measure, through: :survey_item
 
   scope :exclude_boston, lambda {
-                           boston = District.find_by_name("Boston")
-                           where.not(school: boston.schools) if boston.present?
+    includes(school: :district).where.not("district.name": "Boston")
                          }
 
   scope :averages_for_grade, lambda { |survey_items, school, academic_year, grade|
