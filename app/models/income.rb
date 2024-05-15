386cecf68020cfdb8f4f986a0ad1af120eb8bc6a
Nelson Jovel <memoryman51@hotmail.com>
2024-05-15 14:18:45 -0700

    chore: account for #N/A when parsing income

--- app/models/income.rb
+++ app/models/income.rb
@@ -14,7 +14,7 @@ class Income < ApplicationRecord
       "Economically Disadvantaged - Y"
     in /Not\s*Eligible|false|0/i
       "Economically Disadvantaged - N"
-    in /Unknown/i
+    in %r{^#*N/*A$|Unknown}i
       "Unknown"
     else
       puts "************************************"
