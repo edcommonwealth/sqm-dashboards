359c0e2fc02043df3f4a25ee6188b2b66ac61198
Nelson Jovel <memoryman51@hotmail.com>
2024-06-24 13:47:28 -0700

    chore: handle unknowns when parsing records

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -19,6 +19,8 @@ class Gender < ApplicationRecord
       99
     when %r{^#*N/*A$}i
       99
+    when /Unknown|99/i
+      99
     else
       puts "************************************"
       puts "********      ERROR       **********"
