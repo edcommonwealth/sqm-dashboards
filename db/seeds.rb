76b53ef1e4be3248285dc334e91a046207fa61e2
rebuilt <memoryman51@hotmail.com>
2025-01-23 10:56:36 -0800

    feat: add 2024-25 academic year

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require "#{Rails.root}/app/lib/seeder"
 seeder = Seeder.new
 
 seeder.seed_academic_years "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22", "2022-23",
-                           "2023-24 Fall", "2023-24 Spring"
+                           "2023-24 Fall", "2023-24 Spring", "2024-25"
 seeder.seed_districts_and_schools Rails.root.join("data", "master_list_of_schools_and_districts.csv")
 seeder.seed_sqm_framework Rails.root.join("data", "sqm_framework.csv")
 seeder.seed_demographics Rails.root.join("data", "demographics.csv")
