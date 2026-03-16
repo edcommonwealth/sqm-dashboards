12e7725c1839c0ffac3395e67ca5f98b23f22a15
rebuilt <memoryman51@hotmail.com>
2026-03-16 13:07:40 -0700

    Copy csv files to a local folder during processing to avoid sftp timeout errors.  Delete local files after loading to database

--- app/services/sftp/directory.rb
+++ app/services/sftp/directory.rb
@@ -6,6 +6,11 @@ module Sftp
     def self.open(path: '/data/survey_responses/clean', &block)
       sftp_url = ENV['SFTP_URL']
       uri = URI.parse(sftp_url)
+      local_dir = Rails.root.join('tmp', 'uploads')
+      FileUtils.mkdir_p(local_dir)
+
+      downloaded_files = []
+
       Net::SFTP.start(uri.host, uri.user, password: uri.password, keepalive: true, keepalive_interval: 5) do |sftp|
         sftp.dir.foreach(path) do |entry|
           next unless entry.file?
@@ -13,7 +18,21 @@ module Sftp
           filename = entry.name
           puts filename
 
-          sftp.file.open(filepath(path:, filename:), 'r', &block)
+          local_path = local_dir.join(filename)
+          sftp.download!(filepath(path:, filename:), local_path.to_s)
+          downloaded_files << local_path
+        end
+      end
+
+      begin
+        downloaded_files.each do |file_path|
+          ::File.open(file_path, 'r') do |file|
+            block.call(file)
+          end
+        end
+      ensure
+        downloaded_files.each do |file_path|
+          ::File.delete(file_path) if ::File.exist?(file_path)
         end
       end
     end
