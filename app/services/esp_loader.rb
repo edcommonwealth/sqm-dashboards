bef43133cac6d9da05692305afcf1a972809fb11
rebuilt <memoryman51@hotmail.com>
2026-04-02 21:24:35 -0700

    ECP-232 Triton teacher response.  Update esp_count spreadsheet and handle new column names

--- app/services/esp_loader.rb
+++ app/services/esp_loader.rb
@@ -3,17 +3,21 @@ require "csv"
 class EspLoader
   def self.load_data(filepath:)
     respondents = []
+    row_count = 1
     CSV.parse(File.read(filepath), headers: true) do |row|
       row = EspRowValues.new(row:)
-      next unless row.school.present? && row.academic_years.size.positive?
+      row_count += 1
+      next puts("Loading esp counts.  Skipping row #{row_count}") unless row.school.present? && row.academic_years.size.positive?
 
       respondents.concat(create_esp_entry(row:))
     end
 
-    Respondent.import respondents, batch_size: 1000, on_duplicate_key_update: [:total_esp]
+    Respondent.import respondents.compact, batch_size: 1000, on_duplicate_key_update: [:total_esp]
   end
 
   def self.create_esp_entry(row:)
+    return nil if row.school.nil?
+
     row.academic_years.map do |academic_year|
       respondent = Respondent.find_or_initialize_by(school: row.school, academic_year:)
       respondent.total_esp = row.total_esp
@@ -38,12 +42,18 @@ class EspRowValues
 
   def academic_years
     @academic_year ||= begin
-      year = row["Academic Year"]&.strip
+      year = row["Academic Year"]
+      year ||= row["School Year"]
+      return [] if year.nil?
+
+      year = year.strip
       AcademicYear.of_year(year)
     end
   end
 
   def total_esp
-    row["School Totals"]&.strip&.gsub(",", "").to_i
+    total = row["School Totals"]
+    total ||= row["School Total"]
+    total&.strip&.gsub(",", "").to_i
   end
 end
